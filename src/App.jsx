58baa7b6710e01428c3a3fc5574cf29598831f28
Pobarul-islam <pobarul3@gmail.com>
2024-06-25 18:59:58 +0600

    project setup and navigation bar added

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import Navigation from './customer/components/Navigation'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Navigation/>
+      <div>
+        Home
+   </div>
+    </>
+  )
+}
+
+export default App
